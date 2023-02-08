dd3d541ce2460480d36d296b015983457dff854f
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-02-08 15:04:30 -0700

    Add input sliders for size, color

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const gridContainer = document.querySelector('#grid-container');
 
-const gridSize = Math.pow(Number(prompt('Enter number of squares per side. (from 3 - 20)')),2);
-const gridColor = prompt('Enter pen of color');
+// const gridSize = Math.pow(Number(prompt('Enter number of squares per side. (from 3 - 20)')),2);
+const gridSlider = document.getElementById('grid-slider');
+const gridSliderValue = document.getElementById('grid-slider-value');
+gridSliderValue.innerText = gridSlider.value;
+gridSlider.oninput = function () {
+    gridSliderValue.innerText = this.value;
+}
+
+const gridSize = Number(gridSliderValue.innerText) * Number(gridSliderValue.innerText) ;
+const squareHeight = Number(gridSliderValue.innerText);
+
+const colorSlider = document.getElementById('color-slider');
+const colorSliderValue = document.getElementById('color-slider-value');
+colorSliderValue.innerText = colorSlider.value;
+colorSlider.oninput = function () {
+    colorSliderValue.innerText = this.value;
+}
+const gridColor = colorSliderValue.innerText;
 
 for (let i = 0; i < gridSize; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
-    console.log(gridSquare.style.height = (600 / Math.sqrt(gridSize)+'px'));
-    console.log(gridSquare.style.width = (600 / Math.sqrt(gridSize)+'px'));
+    gridSquare.style.height = (500 / squareHeight+'px');
+    gridSquare.style.width = (500 / squareHeight+'px');
+
     gridContainer.appendChild(gridSquare);
 
     const gridCollection = document.querySelectorAll('.grid-square');
