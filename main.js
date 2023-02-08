5841b8e946dfbe6d76db968ba2379e11f7e18c86
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-02-08 16:41:40 -0700

    Impliment grid for canvas to fix bug from flexbox

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const gridContainer = document.querySelector('#grid-container');
 
-// const gridSize = Math.pow(Number(prompt('Enter number of squares per side. (from 3 - 20)')),2);
 const gridSlider = document.getElementById('grid-slider');
 const gridSliderValue = document.getElementById('grid-slider-value');
-gridSliderValue.innerText = gridSlider.value;
+gridSliderValue.innerText = gridSlider.value + ' x ' + gridSlider.value;
 gridSlider.oninput = function () {
-    gridSliderValue.innerText = this.value;
+    gridSliderValue.innerText = this.value + ' x ' + this.value;
 }
 
-const gridSize = Number(gridSliderValue.innerText) * Number(gridSliderValue.innerText) ;
-const squareHeight = Number(gridSliderValue.innerText);
+const gridSize = Number(gridSlider.value) * Number(gridSlider.value) ;
+const squareHeight = Number(gridSlider.value);
 
 const colorSlider = document.getElementById('color-slider');
 const colorSliderValue = document.getElementById('color-slider-value');
@@ -17,16 +16,14 @@ colorSliderValue.innerText = colorSlider.value;
 colorSlider.oninput = function () {
     colorSliderValue.innerText = this.value;
 }
+
 const gridColor = colorSliderValue.innerText;
 
 for (let i = 0; i < gridSize; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
-    gridSquare.style.height = (500 / squareHeight+'px');
-    gridSquare.style.width = (500 / squareHeight+'px');
-
     gridContainer.appendChild(gridSquare);
-
+    gridContainer.style.setProperty(`grid-template-columns`,`repeat(${Number(gridSlider.value)},1fr)`);
     const gridCollection = document.querySelectorAll('.grid-square');
     gridCollection.forEach(div => {
         div.addEventListener('mouseover',(e) => {
